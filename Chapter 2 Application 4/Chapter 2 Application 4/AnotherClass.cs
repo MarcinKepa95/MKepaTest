538b802a9981e331a628b8d5ff343c9b16fa3bf1
Marcinkepa95 <Marcinkepa12@wp.pl>
2019-07-02 15:44:45 +0200

    Chapter2App4 Ostatnia aplikacja 2.07

---
+++ Chapter 2 Application 4/Chapter 2 Application 4/AnotherClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter_2_Application_4
+{
+    class AnotherClass
+    {
+        public static void main()
+        {
+            MessageBox.Show("Pow!");
+        }
+    }
+}
