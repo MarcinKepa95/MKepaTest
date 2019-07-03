e12a2b462958d041f748ef2c4e0f453c1295c8a6
Marcinkepa95 <Marcinkepa12@wp.pl>
2019-07-03 15:38:00 +0200

    03.07 ostatnie zadania

---
+++ Chapter 4 Application 4/Chapter 4 Application 4/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter_4_Application_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MenuMaker menu = new MenuMaker() { randomizer = new Random() };
+            label1.Text = menu.GetMenuItems();
+            label2.Text = menu.GetMenuItems();
+            label3.Text = menu.GetMenuItems();
+            label4.Text = menu.GetMenuItems();
+            label5.Text = menu.GetMenuItems();
+            label6.Text = menu.GetMenuItems();
+
+        }
+    }
+}
