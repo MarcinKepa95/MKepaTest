4a9d184a11820a3b84b49c6b4c2b923a09eb32cb
Marcinkepa95 <Marcinkepa12@wp.pl>
2019-07-17 14:47:49 +0200

    Go Fish WPF (Do dokończenia (raczej nie))

---
+++ Go Fish WPF!/Go Fish WPF!/Game.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Go_Fish_WPF_
+{
+
+    class Game
+    {
+        private List<Player> players;
+        private Dictionary<Value, Player> books;
+        private Deck stock;
+        private TextBox textBoxOnForm;
+        public Game(string playerName, IEnumerable<string> opponentNames, TextBox textBoxOnForm)
+        {
+            Random random = new Random();
+            this.textBoxOnForm = textBoxOnForm;
+            players = new List<Player>();
+            players.Add(new Player(playerName, random, textBoxOnForm));
+            foreach (string player in opponentNames)
+                players.Add(new Player(player, random, textBoxOnForm));
+            books = new Dictionary<Value, Player>();
+            stock = new Deck();
+            Deal();
+            players[0].SortHand();
+        }
+        private void Deal()
+        {
+            stock.Shuffle();
+                for(int i=0; i<5; i++)
+                    foreach (Player player in players)
+                        player.TakeCard(stock.Deal(0));
+            foreach (Player player in players)
+            {
+                player.PullOutBooks();
+            }
+            // This is where the game starts—this method's only called at the beginning
+            // of the game. Shuffle the stock, deal five cards to each player, then use a
+            // foreach loop to call each player's PullOutBooks() method.
+        }
+        public bool PlayOneRound(int selectedPlayerCard)
+        {
+            
+            Value cardToAskFor = players[0].Peek(selectedPlayerCard).value;
+
+            for(int i=0; i<players.Count; i++)
+            {
+                int cardcount = players[i].CardCount;
+                if (i == 0)
+                    players[0].AskForACard(players, 0, stock, cardToAskFor);
+                else players[i].AskForACard(players, i, stock);
+                if (PullOutBooks(players[i]))
+                {
+                    textBoxOnForm.Text += players[i].Name
+                        + " drew a new hand" + Environment.NewLine;
+                    int card = 1;
+                    while (card <= 5 && stock.Count > 0)
+                    {
+                        players[i].TakeCard(stock.Deal());
+                        card++;
+                    }
+                }
+                else if (cardcount == players[i].CardCount)
+                    players[i].TakeCard(stock.Deal());
+                
+                if(stock.Count==0)
+                {
+                    textBoxOnForm.Text = "The stock is out of cards! Game Over!"+ Environment.NewLine;
+                    return true;
+                }
+            }
+            for(int i=0; i<players.Count; i++)
+            {
+                if (PullOutBooks(players[i]))
+                {
+                    textBoxOnForm.Text += players[i].Name
+                        + " drew a new hand" + Environment.NewLine;
+                    int card = 1;
+                    while (card <= 5 && stock.Count > 0)
+                    {
+                        players[i].TakeCard(stock.Deal());
+                        card++;
+                    }
+                }
+            }
+            players[0].SortHand();
+            return false;
+            // Play one round of the game. The parameter is the card the player selected
+            // from his hand—get its value. Then go through all of the players and call
+            // each one's AskForACard() methods, starting with the human player (who's
+            // at index zero in the Players list—make sure he asks for the selected
+            // card's value). Then call PullOutBooks()—if it returns true, then the
+            // player ran out of cards and needs to draw a new hand. After all the players
+            // have gone, sort the human player's hand (so it looks nice in the form).
+            // Then check the stock to see if it's out of cards. If it is, reset the
+            // TextBox on the form to say, "The stock is out of cards. Game over!" and return
+            // true. Otherwise, the game isn't over yet, so return false.
+        }
+        public bool PullOutBooks(Player player)
+        {
+            IEnumerable<Value> k = player.PullOutBooks();
+            foreach (Value key in k)
+            {
+                books.Add(key, player);
+            }
+            if (player.CardCount == 0)
+                return true;
+            return false;
+            // Pull out a player's books. Return true if the player ran out of cards, otherwise
+            // return false. Each book is added to the Books dictionary. A player runs out of
+            // cards when he’'s used all of his cards to make books—and he wins the game.
+        }
+        public string DescribeBooks()
+        {
+            string describestring="";
+            foreach(Value key in books.Keys)
+            {
+                describestring += books[key].Name + " has a book of " + key+ Environment.NewLine;
+            }
+            return describestring;
+            // Return a long string that describes everyone's books by looking at the Books
+            // dictionary: "Joe has a book of sixes. (line break) Ed has a book of Aces."
+        }
+        public string GetWinnerName()
+        {
+            Dictionary<string, int> winners = new Dictionary<string, int>();
+            int thebiggest = 0;
+            string message = "";
+            bool tie = false;
+
+            foreach (Value value in books.Keys)
+            {
+                if (winners.ContainsKey(books[value].Name))
+                    winners[books[value].Name]++;
+                else
+                    winners.Add(books[value].Name, 1);
+                if (thebiggest < winners[books[value].Name])
+                    thebiggest = winners[books[value].Name];
+            }
+            foreach (string name in winners.Keys)
+            {
+                if (thebiggest < winners[name])
+                    thebiggest = winners[name];
+            }
+            foreach (string name in winners.Keys)
+            {
+                if (winners[name] == thebiggest)
+                    if (String.IsNullOrEmpty(message))
+                        message = name;
+                    else
+                    {
+                        tie = true;
+                        message += " and " + name;
+                    }
+            }
+            if (tie)
+                return "A tie between " + message + " with " + thebiggest + " books";
+            return message + " with " + thebiggest + " books";
+            // This method is called at the end of the game. It uses its own dictionary
+            // (Dictionary<string, int> winners) to keep track of how many books each player
+            // ended up with in the books dictionary. First it uses a foreach loop
+            // on books.Keys—foreach (Values value in books.Keys)—to populate
+            // its winners dictionary with the number of books each player ended up with.
+            // Then it loops through that dictionary to find the largest number of books
+            // any winner has. And finally it makes one last pass through winners to come
+            // up with a list of winners in a string ("Joe and Ed"). If there's one winner,
+            // it returns a string like this: "Ed with 3 books". Otherwise, it returns a
+            // string like this: "A tie between Joe and Bob with 2 books."
+        }
+        // Here are a couple of short methods that were already written for you:
+        public IEnumerable<string> GetPlayerCardNames()
+        {
+            return players[0].GetCardNames();
+        }
+        public string DescribePlayerHands()
+        {
+            string description = "";
+            for (int i = 0; i < players.Count; i++)
+            {
+                description += players[i].Name + " has " + players[i].CardCount;
+                if (players[i].CardCount == 1)
+                    description += " card." + Environment.NewLine;
+                else
+                    description += " cards." + Environment.NewLine;
+            }
+            description += "The stock has " + stock.Count + " cards left.";
+            return description;
+        }
+    }
+}
---
+++ Go Fish WPF!/Go Fish WPF!/Player.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Xaml;
+
+namespace Go_Fish_WPF_
+{
+    class Player
+    {
+        private string name;
+        public string Name { get { return name; } }
+        private Random random;
+        private Deck cards;
+        private TextBox textBoxOnForm;
+        
+        public Player(String name, Random random, TextBox textBoxOnForm)
+        {
+            this.textBoxOnForm = textBoxOnForm;
+            this.name = name;
+            this.random = random;
+            List<Card> cart = new List<Card>();
+            cards = new Deck(cart);
+
+            textBoxOnForm.Text += Name + " has just joined the game" + Environment.NewLine;
+        }
+        public IEnumerable<Value> PullOutBooks()
+        {
+            List<Value> books = new List<Value>();
+            for (int i = 1; i <= 13; i++)
+            {
+                Value value = (Value)i;
+                int howMany = 0;
+                for (int card = 0; card < cards.Count; card++)
+                    if (cards.Peek(card).value == value)
+                        howMany++;
+                if (howMany == 4)
+                {
+                    books.Add(value);
+                    cards.PullOutValues(value);
+                }
+            }
+            return books;
+        }
+        public Value GetRandomValue()
+        {
+            int v;
+            do
+            {
+                v = random.Next(1, 13);
+            } while (!cards.ContainsValue((Value)v));
+            return (Value)v;
+        }
+        public Deck DoYouHaveAny(Value value)
+        {
+            Deck exactCards = cards.PullOutValues(value);
+            if (exactCards.Count > 0)
+                textBoxOnForm.Text += Name + " has " + exactCards.Count + " " + Card.Plural(value) + "\r\n";
+            SortHand();
+            return exactCards;
+        }
+        public void AskForACard(List<Player> players, int myIndex, Deck stock)
+        {
+            AskForACard(players, myIndex, stock, GetRandomValue());
+        }
+        public void AskForACard(List<Player> players, int myIndex, Deck stock, Value value)
+        {
+            textBoxOnForm.Text += Name + " asks if anyone has a " + value+Environment.NewLine;
+            int DeckCount = CardCount;
+            foreach(Player player in players)
+            {
+                if (player != this)
+                {
+                    Deck deck = player.DoYouHaveAny(value);
+                    while (deck.Count > 0)
+                    {
+                        DeckCount++;
+                        TakeCard(deck.Deal(0));
+                    }
+                }
+            }
+            if (DeckCount == 0)
+            {
+                textBoxOnForm.Text += Name + " had to draw from the stock";
+                TakeCard(stock.Deal(0));
+            }
+            SortHand();
+        }
+
+        public int CardCount { get { return cards.Count; } }
+        public void TakeCard(Card card) { cards.Add(card); }
+        public IEnumerable<string> GetCardNames() { return cards.getCardsName(); }
+        public Card Peek(int cardNumber) { return cards.Peek(cardNumber); }
+        public void SortHand() { cards.SortByValue(); }
+    }
+    public partial class Card
+    {
+        public static string Plural(Value value)
+        {
+            if (value == Value.Six)
+                return "Sixes";
+            else
+                return value.ToString() + "s";
+        }
+    }
+}
